539f2ec118865a0ddf39d3c61b2c1251b0bdb74a
SeungJi20 <lina6709@naver.com>
2025-09-01 12:47:10 +0900

    경로 추가

--- src/main/java/com/example/order_system/common/auth/SecurityConfig.java
+++ src/main/java/com/example/order_system/common/auth/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig { // 이게 필터체인
 
     private CorsConfigurationSource corsConfiguration(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://www.seung12.shop"));
         configuration.setAllowedMethods(Arrays.asList("*")); // 모든 HTTP(get, post 등) 메서드 허용
         configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더요소(Authorization 등) 허용
         configuration.setAllowCredentials(true); // 자격 증명 허용
